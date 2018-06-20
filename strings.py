7e6b58c7d7b133992ceaea8bd412db4f1f9e7a4c
tracine3636 <39446050+tracine3636@users.noreply.github.com>
2018-06-20 22:05:23 +0000

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,14 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:43:06) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> word = tree
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    word = tree
+NameError: name 'tree' is not defined
+>>> word = "tree"
+>>> word
+'tree'
+>>> word = "two trees"
+>>> word
+'two trees'
+>>> 
