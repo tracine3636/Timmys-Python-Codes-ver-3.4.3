766eeda2b39b46aa3e04c2031633a9ff77aeeddc
tracine3636 <39446050+tracine3636@users.noreply.github.com>
2018-06-20 22:06:52 +0000

    Add files via upload

---
+++ list2.py
@@ -0,0 +1,27 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:43:06) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> testList = {1,2,3}
+>>> testList
+{1, 2, 3}
+>>> testList = {1, 2, 3, 4}
+>>> testList
+{1, 2, 3, 4}
+>>> testList = 1, 2, 3
+>>> testList
+(1, 2, 3)
+>>> 
+word =25
+>>> word = 1.5
+>>> number = 25
+>>> 
+>>> word
+1.5
+>>> number
+25
+>>> 
+>>> list1= {number, word}
+>>> list 1
+SyntaxError: invalid syntax
+>>> list1
+{1.5, 25}
+>>> 
