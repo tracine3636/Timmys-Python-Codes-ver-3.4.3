a9548026074267d0158b1a864e972646944e8cc1
tracine3636 <39446050+tracine3636@users.noreply.github.com>
2018-06-20 21:19:19 +0000

    Update My Saturday.py

--- My Saturday.py
+++ My Saturday.py
@@ -1,5 +1,3 @@
-
-
 print("What did you do on Saturday?")
 mySaturday = input("Did you practice programming Python? or did you practice on the Ukulele?")
 if(mySaturday == 'Python'): print('Awesome!')
