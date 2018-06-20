7900f38ab5574406f8e0a46336a3b2919ac8eabe
tracine3636 <39446050+tracine3636@users.noreply.github.com>
2018-06-20 21:18:47 +0000

    Add files via upload

---
+++ My Saturday.py
@@ -0,0 +1,7 @@
+
+
+print("What did you do on Saturday?")
+mySaturday = input("Did you practice programming Python? or did you practice on the Ukulele?")
+if(mySaturday == 'Python'): print('Awesome!')
+elif(mySaturday == 'Ukulele'): print('Sweet!')
+else: print('Cool Beans')
